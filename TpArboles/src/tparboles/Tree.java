c3ca702c6dbd170e0fb9f121b8eebeac074ce78b
Ilari Nicolas <ilarinico40@gmail.com>
2023-05-03 14:21:15 -0300

    agrego grafos

--- TpArboles/src/tparboles/Tree.java
+++ TpArboles/src/tparboles/Tree.java
@@ -4,6 +4,9 @@
  */
 package tparboles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ilari
@@ -45,12 +48,38 @@ public class Tree {
         return value==null;
     }
     
+    public void printInOrder(){
+        if(left!=null)
+            left.printInOrder();
+        else
+            System.out.println("-");
+        System.out.println(value);
+        if(right!=null)
+            right.printInOrder();
+        else
+            System.out.println("-");
+        
+    }
+    
+    public void printPosOrder(){
+        if(left!=null)
+            left.printPosOrder();
+        else
+            System.out.println("-");
+        if(right!=null)
+            right.printPosOrder();
+        else
+            System.out.println("-");
+        System.out.println(value);
+    }
+    
     public void printPreOrder(){
+        
+        System.out.println(value);
         if(left!=null)
             left.printPreOrder();
         else
             System.out.println("-");
-        System.out.println(value);
         if(right!=null)
             right.printPreOrder();
         else
@@ -58,31 +87,203 @@ public class Tree {
     }
     
     public int getHeight(){
+        return (this.getHeightPlusOne()-1);
+    }
+    
+    private int getHeightPlusOne(){
         int height=0;
-        
         if(value!=null){
             if((left!=null)&&(right!=null)){
-                if(left.getHeight()>right.getHeight()){
-                    height+=left.getHeight();
+                if(left.getHeightPlusOne()>right.getHeightPlusOne()){
+                    height+=left.getHeightPlusOne();
                 }
                 else{
-                    height+=right.getHeight();
+                    height+=right.getHeightPlusOne();
                 }
             }
             else if(left!=null){
-                height+=left.getHeight();
+                height+=left.getHeightPlusOne();
             }
             else if(right!=null){
-                height+=right.getHeight();
+                height+=right.getHeightPlusOne();
             }
-
             height++;
         }
         return height;
     }
+
+   public int returnSum(){
+       int sum=0;
+       if (value!=null)
+        sum=value;
+       if(right!=null)
+           sum+=right.returnSum();
+       if(left!=null)
+        sum+=left.returnSum();
+       
+       return sum;
+   }
     
-    public void delete(Integer e){
-        
+   public List numerosMayor(int n){
+       ArrayList list=new ArrayList();
+       if(value!=null){
+           if(left!=null)
+               list.addAll(left.numerosMayor(n));
+           if(value>n)
+               list.add(value);
+           if(right!=null)
+               list.addAll(right.numerosMayor(n));
+       }
+       return list; 
+       
+   }   
+    public int returnSuc(Tree parent,String dir){
+        if((left==null)&&(right==null)){
+            int val=value;
+            if(dir=="iz"){
+                parent.left=null;
+            }
+            else
+                parent.right=null;
+            value=null;
+            return val;
+        }
+            
+        if(left.left==null){
+            if(left.right!=null)
+            {
+                int val=left.value;
+                Tree aux=left;
+                this.left=left.right;
+                aux.right=null;
+                return val;
+            }
+            else{
+                int val=left.value;
+                left=null;
+                return val;
+            }
+        }
+        else return left.returnSuc(this,"iz");
+    }
+    
+    private void reasignate(Tree n){
+        this.value=n.value; //hace el puente entre los nodos
+        Tree aux=n;
+        Tree auxl=aux.left;
+        Tree auxr=aux.right;
+        n.value=null;
+        n.left=null;
+        n.right=null;
+        this.left=auxl;
+        this.right=auxr;
+    }
+    
+    
+    
+    public boolean delete(Integer i){
+        if(value!=null){
+            if(value.equals(i)){
+                if((right!=null)&&(left!=null)){
+                     this.value=right.returnSuc(this,"der"); //cuando tenes que borrar uno que tiene dos hijos, lo tenes que sustituir por el
+                                                    //predecesor o sucedor, el sucesor es el mas chico del arbol derecho
+                     return true;
+                }
+                else if(right!=null){
+                    this.reasignate(right); //cuando tenes que borrar derecha y tiene hijos, los reasigna
+                    return true;
+                }
+                else if(left!=null){
+                    this.reasignate(left);
+                    return true;
+                }
+                else if((right==null)&&(left==null)){
+                    this.value=null; //izquierda y derecha son nulos, mi valor es nulo
+                    return true;
+                }
+            }
+            else if(i<value)
+                return left.delete(i);
+            else
+                return right.delete(i);
+        }
+        return false;
+    }
+    
+    
+    public List getLongestBranch(){
+        ArrayList list=new ArrayList();
+        if(value!=null)
+            list.add(value);
+        if((left!=null)&&(right!=null)){
+            if(left.getHeight()>right.getHeight())
+                list.addAll(left.getLongestBranch());
+            else
+                list.addAll(right.getLongestBranch());
+        }
+        else if(left!=null)
+            list.addAll(left.getLongestBranch());
+        else if(right!=null)
+            list.addAll(right.getLongestBranch());
+        return list;
+    }
+    
+    
+    
+    public List getFrontier(){
+        ArrayList list=new ArrayList();
+        if((left==null)&&(right==null)){
+            if(value!=null)
+                list.add(value);
+        }
+        else{
+            if(left!=null)
+                list.addAll(left.getFrontier());
+            if(right!=null)
+                list.addAll(right.getFrontier());
+        }
+        return list;
+    }
+    
+    public Integer getMaxElem(){
+        if(value!=null){
+            int max=value;
+            if((left!=null)&&(left.getMaxElem()>max))
+                max=left.getMaxElem();
+            else if((right!=null)&&(right.getMaxElem()>max)){
+                max=right.getMaxElem();
+            }
+            return max;
+        }
+        else
+            return -1;
+    }
+    
+    public Integer getMaxElement(){
+        if(value!=null){
+            
+            if(right!=null){
+                return right.getMaxElement();
+            }
+            else
+                return value;
+        }
+        return -1; 
+    }
+    
+    public List getElemAtLevel(int l){
+        ArrayList list=new ArrayList();
+        if(value!=null){
+            if(l>0){
+                if(left!=null)
+                    list.addAll(left.getElemAtLevel(l-1));
+                if(right!=null)
+                    list.addAll(right.getElemAtLevel(l-1));
+            }
+            else
+                list.add(value);
+        }
+        return list;
     }
     
     public void add(Integer newValue) {
--- TpGrafos/src/tpgrafos/TpGrafos.java
+++ TpGrafos/src/tpgrafos/TpGrafos.java
@@ -2,30 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tparboles;
+package tpgrafos;
 
 /**
  *
  * @author ilari
  */
-public class TpArboles {
+public class TpGrafos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Tree a=new Tree(2);
-        a.add(7);
-        a.add(4);
-        a.add(5);
-        a.add(6);
-        a.add(9);
-        a.add(8);
-        a.add(10);
-        a.add(1);
-        a.add(0);
-        a.add(-1);
-        System.out.println(a.numerosMayor(4));
+        // TODO code application logic here
     }
     
 }
